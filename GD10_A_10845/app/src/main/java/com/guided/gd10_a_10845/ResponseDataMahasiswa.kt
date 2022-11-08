499bee175f015ed5c96b6210920b25503168112d
RenaSuban <irenesianrsuban@gmail.com>
2022-11-08 20:58:50 +0700

    Guided 10

---
+++ GD10_A_10845/app/src/main/java/com/guided/gd10_a_10845/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.guided.gd10_a_10845
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
